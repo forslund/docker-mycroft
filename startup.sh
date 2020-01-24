d45705254baa79f35dbc4654788f182f0586248a
Åke Forslund <ake.forslund@gmail.com>
2020-01-24 17:15:56 +0100

    Show core's SHA and delete skills sandbox

--- startup.sh
+++ startup.sh
@@ -3,4 +3,6 @@ source /opt/mycroft-core/.venv/bin/activate
 /opt/mycroft-core/start-mycroft.sh all
 python -m test.integrationtests.voigt_kampff.test_setup -c ~/.mycroft/test.yml
 cd /opt/mycroft-core/test/integrationtests/voigt_kampff
+echo "Mycroft-core:  $(git log --pretty=format:%h -n 1)"
 behave
+rm -rf ~/.mycroft/skills
